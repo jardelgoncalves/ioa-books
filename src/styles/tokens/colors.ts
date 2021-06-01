6912a60ac72962c600775501a9825157ee9ec292
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-31 21:12:36 -0300

    feat: add colors config

---
+++ src/styles/tokens/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#B22E6F',
+  primaryDark: '#AB2680',
+  black: '#333333',
+  grey: '#999999',
+  white: '#FFFFFF',
+  bgInput: '#00000052',
+  borderColor: '#33333333',
+} as const
