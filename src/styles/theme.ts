a4823bab451a931cfdbcf90ff5be8934cf27c669
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-06-01 21:14:24 -0300

    feat: export theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import {
+  breakpoints,
+  colors,
+  shapes,
+  sizes,
+  spacing,
+  typography,
+} from './tokens'
+
+export const theme = {
+  breakpoints,
+  colors,
+  shapes,
+  sizes,
+  spacing,
+  typography,
+}
