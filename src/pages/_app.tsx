9ea3bd8b1b1774a9447b655c2058a309d4215531
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-06-01 21:14:56 -0300

    feat: add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global.styles'
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { theme } from '../styles/theme'
+
 type AppProps = {
-  Component: React.FC;
-  props: any;
-};
+  Component: React.FC
+  props: any
+}
 
 export default function App({ Component, props }: AppProps) {
-  return <Component {...props} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Ioa Books</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...props} />
+    </ThemeProvider>
+  )
 }
