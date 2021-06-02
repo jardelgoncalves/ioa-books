62157f8971fb891d1604bedf48c2502107441919
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-06-01 23:58:59 -0300

    feat: add hook to apply theme and global style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../src/styles/theme'
+import { GlobalStyle } from '../src/styles/global.styles'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +11,14 @@ export const parameters = {
     },
   },
 }
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+
+export const decorators = [withThemeProvider]
