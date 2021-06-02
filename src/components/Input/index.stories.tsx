cc1c210f9d396132ea411a7efe895f71e4f5e260
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-06-02 01:18:47 -0300

    feat: adddoc to input component

---
+++ src/components/Input/index.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+
+import { Input, InputProps } from './'
+
+export default {
+  title: 'Example/Input',
+  component: Input,
+  argTypes: {},
+} as Meta
+
+const Template: Story<InputProps> = (args) => <Input {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  label: 'Email',
+  placeholder: 'test@test.com',
+}
