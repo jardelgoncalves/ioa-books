d849abeb69ea6c8497c85f779e73174842117584
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-06-02 01:17:43 -0300

    refactor: update doc button

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,37 +1,31 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react";
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
 
-import { Button, ButtonProps } from "./";
+import { Button, ButtonProps } from './'
 
 export default {
-  title: "Example/Button",
+  title: 'Example/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: "color" },
+    // backgroundColor: { control: 'color' },
   },
-} as Meta;
+} as Meta
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
-  primary: true,
-  label: "Button",
-};
+  label: 'Entrar',
+}
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
+// export const Large = Template.bind({})
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// }
 
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
-};
+// export const Small = Template.bind({})
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// }
