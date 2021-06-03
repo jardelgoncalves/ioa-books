25673d8cdef446a654173a694f33839be8729a93
jardel <jardelgoncalves1996@gmail.com>
2021-06-03 20:56:26 -0300

    feat: add icon sizes

--- src/styles/tokens/shapes.ts
+++ src/styles/tokens/shapes.ts
@@ -7,9 +7,17 @@ export const shapes = {
     xlg: '2rem',
   },
   buttonSizes: {
+    '2xs': '2rem',
     xs: '2.25rem',
     sm: '2.5rem',
     md: '3rem',
     lg: '3.5rem',
+  },
+  iconSizes: {
+    xs: '0.5rem',
+    sm: '0.75rem',
+    md: '1rem',
+    lg: '1.5rem',
+    xl: '2rem',
   },
 } as const
