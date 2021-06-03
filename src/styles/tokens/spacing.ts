5af926ac2edbe91077f5fde54a767424ae5c8231
jardel <jardelgoncalves1996@gmail.com>
2021-06-03 20:56:43 -0300

    refactor: change sizes

--- src/styles/tokens/spacing.ts
+++ src/styles/tokens/spacing.ts
@@ -4,7 +4,7 @@ export const spacing = {
   sm: '1rem',
   md: '1.5rem',
   lg: '2rem',
-  xl: '3rem',
-  '2xl': '4rem',
-  '3xl': '5.5rem',
+  xl: '2.5rem',
+  '2xl': '3rem',
+  '3xl': '3.5rem',
 } as const
