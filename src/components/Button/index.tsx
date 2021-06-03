be7ceb2833ea2e7eba0823685582f4b701d07908
jardel <jardelgoncalves1996@gmail.com>
2021-06-03 20:54:06 -0300

    feat: add outline style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,37 @@
 import * as S from './styles'
 
 export interface ButtonProps {
-  size?: 'xs' | 'sm' | 'md' | 'lg'
+  size?: '2xs' | 'xs' | 'sm' | 'md' | 'lg'
   type?: 'button' | 'submit' | 'reset'
+  outline?: boolean
+  bg?: 'primary' | 'black' | 'grey' | 'white'
   width?: number
-  label: string
+  label?: string
   onClick?: () => void
+  children?: React.ReactNode
 }
 
-export const Button = ({ size, width, label, onClick, type }: ButtonProps) => {
+export const Button = ({
+  size,
+  width,
+  label,
+  onClick,
+  type,
+  outline,
+  bg,
+  children,
+}: ButtonProps) => {
   return (
     <S.ButotnWrapper
       size={size}
+      outline={outline}
+      bg={bg}
       width={width}
       onClick={onClick}
       type={type || 'button'}
     >
       {label}
+      {children}
     </S.ButotnWrapper>
   )
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,19 +1,40 @@
 import styled from 'styled-components'
+import { rgba } from 'polished'
 
 export interface ButtonProps {
-  size?: 'xs' | 'sm' | 'md' | 'lg'
+  outline?: boolean
+  bg?: 'primary' | 'black' | 'grey' | 'white'
+  size?: '2xs' | 'xs' | 'sm' | 'md' | 'lg'
   width?: number
 }
 export const ButotnWrapper = styled.button<ButtonProps>`
-  height: ${({ theme, size }) => theme.shapes.buttonSizes[size || 'md']};
-  width: ${({ width }) => `${width || 85}px`};
-  background-color: ${({ theme }) => theme.colors.white};
-  border: none;
-  border-radius: ${({ width }) => `${width || 85}px`};
+  ${({ outline, theme, size, bg, width }) => {
+    if (outline) {
+      return `
+        height: ${theme.shapes.buttonSizes[size || 'md']};
+        width: ${theme.shapes.buttonSizes[size || 'md']};
+        background-color: transparent;
+        border-radius: ${theme.shapes.buttonSizes[size || 'md']};
+        border: 1px solid ${rgba(theme.colors[bg || 'white'], 0.2)};
+      `
+    }
+    return `
+      height: ${theme.shapes.buttonSizes[size || 'md']};
+      width: ${width || 85}px;
+      border: none;
+      border-radius: ${width || 85}px;
+      background-color: ${theme.colors[bg || 'white']};
+    `
+  }}
   color: ${({ theme }) => theme.colors.primary};
   font-weight: ${({ theme }) => theme.typography.weight.medium};
   font-size: ${({ theme }) => theme.typography.sizes.base};
   display: flex;
   align-items: center;
   justify-content: center;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `
