01cfce92bf636cc5ea7522534bab55c6ca647c2c
jardel <jardelgoncalves1996@gmail.com>
2021-06-03 20:54:49 -0300

    feat: add style to header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components'
+
+const fontCommons = css`
+  color: ${({ theme }) => theme.colors.black};
+  font-weight: ${({ theme }) => theme.typography.weight.regular};
+  font-size: ${({ theme }) => theme.typography.sizes.xs};
+  line-height: ${({ theme }) => theme.typography.lineHeight.sm};
+`
+
+export const Header = styled.header`
+  position: relative;
+  width: 100%;
+  height: ${({ theme }) => theme.sizes.desktop.navbar};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: ${({ theme }) => theme.spacing.xl};
+`
+
+export const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const UserMessage = styled.span`
+  ${fontCommons}
+`
+
+export const UserName = styled.span`
+  ${fontCommons}
+  font-weight: ${({ theme }) => theme.typography.weight.medium};
+  margin-right: ${({ theme }) => theme.spacing.md};
+`
