ed198dcfe294478d07da1e7b5acfac86b1a141db
jardel <jardelgoncalves1996@gmail.com>
2021-06-03 20:54:38 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { Logo } from '../Logo'
+import { Button } from '../Button'
+import * as S from './styles'
+import { Icon } from '../Icon'
+
+interface User {
+  name: string
+}
+
+export interface HeaderProps {
+  user: User
+}
+
+export const Header = ({ user }: HeaderProps) => {
+  return (
+    <S.Header>
+      <Logo themeColor="black" />
+      <S.UserWrapper>
+        <S.UserMessage>
+          Bem vindo, <S.UserName>{user.name}!</S.UserName>
+        </S.UserMessage>
+        <Button outline bg="black" size="2xs">
+          <Icon name="logout" size="md" />
+        </Button>
+      </S.UserWrapper>
+    </S.Header>
+  )
+}
