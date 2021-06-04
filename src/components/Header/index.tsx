b106f89eaaeaf65e28eef781626ffe79dc3c9ecf
jardel <jardelgoncalves1996@gmail.com>
2021-06-03 23:20:49 -0300

    feat: add scroll effect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { Logo } from '../Logo'
 import { Button } from '../Button'
 import * as S from './styles'
 import { Icon } from '../Icon'
+import { useEffect, useState } from 'react'
 
 interface User {
   name: string
@@ -12,12 +13,30 @@ export interface HeaderProps {
 }
 
 export const Header = ({ user }: HeaderProps) => {
+  const [headerClass, setHeaderClass] = useState('')
+  const onScroll = () => {
+    const currentYPos = window.scrollY
+    const limitYPos = 40
+    const isScrollingUp = currentYPos > limitYPos
+    if (isScrollingUp) {
+      setHeaderClass('--scroll')
+      return
+    }
+    setHeaderClass('')
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll, false)
+    return () => {
+      window.removeEventListener('scroll', onScroll, false)
+    }
+  }, [])
   return (
-    <S.Header>
+    <S.Header className={headerClass}>
       <Logo themeColor="black" />
       <S.UserWrapper>
-        <S.UserMessage>
-          Bem vindo, <S.UserName>{user.name}!</S.UserName>
+        <S.UserMessage className="user-message">
+          Bem vindo, <S.UserName>{user?.name ?? 'USUÁRIO'}!</S.UserName>
         </S.UserMessage>
         <Button outline bg="black" size="2xs">
           <Icon name="logout" size="md" />
