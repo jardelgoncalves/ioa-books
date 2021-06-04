c82b5d9ff613ba0a760e30642cf774f13df4fba7
jardel <jardelgoncalves1996@gmail.com>
2021-06-04 12:27:00 -0300

    feat: add card book component

---
+++ src/components/CardBook/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+import { Book } from '../../interfaces/book'
+import * as S from './styles'
+
+export interface CardBookProps {
+  book: Book
+  onClick?: (book: Book) => void
+}
+
+export const CardBook = ({ book, onClick }: CardBookProps) => {
+  return (
+    <S.Box onClick={() => onClick(book)}>
+      <S.CoverContainer>
+        <Image height={192} width={325} src={book.imageUrl} alt={book.title} />
+      </S.CoverContainer>
+      <S.Description>
+        <S.Title>{book.title}</S.Title>
+        <S.Author>{book.authors}</S.Author>
+        <S.Info>
+          <span>{book.pageCount} paginas</span>
+          <span>{book.publisher}</span>
+          <span>Publicado em {book.published}</span>
+        </S.Info>
+      </S.Description>
+    </S.Box>
+  )
+}
