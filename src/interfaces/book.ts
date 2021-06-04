81281a854a0eef0ee19bb56c7719b6338521c2e7
jardel <jardelgoncalves1996@gmail.com>
2021-06-04 12:27:09 -0300

    feat: add book interface

---
+++ src/interfaces/book.ts
@@ -0,0 +1,14 @@
+export interface Book {
+  id: string
+  authors: string
+  title: string
+  description: string
+  pageCount: number
+  language: string
+  category: string
+  imageUrl: string
+  isbn10: string
+  isbn13: string
+  publisher: string
+  published: number
+}
