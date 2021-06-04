dfdacb66bb67ba767b4b099706f7889b2fae7743
jardel <jardelgoncalves1996@gmail.com>
2021-06-04 12:26:54 -0300

    feat: add style to card book component

---
+++ src/components/CardBook/styles.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+
+export const Box = styled.button`
+  max-width: 17rem;
+  width: 100%;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 6px 24px 0px ${({ theme }) => theme.colors.boxShadowPurple};
+  padding: ${({ theme }) => theme.spacing.sm};
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  border: 0;
+  transition: box-shadow 0.3s;
+  &:hover {
+    box-shadow: 0px 16px 80px 0px
+      ${({ theme }) => theme.colors.boxShadowPurpleDark};
+  }
+`
+
+export const CoverContainer = styled.div`
+  height: 100%;
+  width: 9.2875rem;
+  box-shadow: 0px 6px 9px 0px ${({ theme }) => theme.colors.boxShadowBlack};
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding-left: 0.75rem;
+  text-align: left;
+`
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: ${({ theme }) => theme.typography.sizes.sm};
+  font-weight: ${({ theme }) => theme.typography.weight.medium};
+  line-height: ${({ theme }) => theme.typography.lineHeight.base};
+`
+
+export const Author = styled.h3`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${({ theme }) => theme.typography.sizes.xs};
+  font-weight: ${({ theme }) => theme.typography.weight.regular};
+  line-height: ${({ theme }) => theme.typography.lineHeight.base};
+`
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100%;
+
+  span {
+    padding: 0;
+    color: ${({ theme }) => theme.colors.grey};
+    font-size: ${({ theme }) => theme.typography.sizes.xs};
+    font-weight: ${({ theme }) => theme.typography.weight.regular};
+    line-height: 1.25rem;
+  }
+`
