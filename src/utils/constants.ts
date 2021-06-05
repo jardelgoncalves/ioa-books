486c285eca722703f67188deae97c194f1d332e5
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 00:17:36 -0300

    feat: add context to cookies

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const BASE_URL = 'https://books.ioasys.com.br/api/v1'
+export const USER_COOKIES = '@books-ioasys.user'
+export const TOKEN_COOKIES = '@books-ioasys.token'
+export const MAX_AGE_COOKIES = 60 * 60 * 24 // 1 day
