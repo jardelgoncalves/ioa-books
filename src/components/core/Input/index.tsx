c9cb7230ea4b61a12b4c31642b55dcbff71b8f30
jardel <jardelgoncalves1996@gmail.com>
2021-06-04 21:03:54 -0300

    refactor: re-create component with forwardRef

--- src/components/core/Input/index.tsx
+++ src/components/core/Input/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import * as S from './styles'
 
 export interface InputProps {
@@ -10,28 +11,31 @@ export interface InputProps {
   onchange?: () => void
 }
 
-export const Input = ({
-  label,
-  name,
-  type,
-  placeholder,
-  children,
-  value,
-  onchange,
-}: InputProps) => {
-  return (
-    <S.Container>
-      <S.InputWrapper>
-        <S.Label>{label}</S.Label>
-        <S.Input
-          placeholder={placeholder}
-          value={value}
-          name={name}
-          type={type || 'text'}
-          onChange={onchange}
-        />
-      </S.InputWrapper>
-      {children}
-    </S.Container>
-  )
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    { label, name, type, placeholder, children, value, onchange, ...args },
+    ref,
+  ) => {
+    return (
+      <S.Container>
+        <S.InputWrapper>
+          <S.Label>{label}</S.Label>
+          <S.Input
+            ref={ref}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            type={type || 'text'}
+            onChange={onchange}
+            {...args}
+          />
+        </S.InputWrapper>
+        {children}
+      </S.Container>
+    )
+  },
+)
+
+Input.displayName = 'Input'
+
+export { Input }
