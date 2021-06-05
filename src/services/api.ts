541b147f1aa8b11e7f2e5cbe2ced463a5c84fabb
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 00:16:55 -0300

    feat: add context to using in server side

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,30 @@
 import axios from 'axios'
-import { BASE_URL } from '../utils/constants'
+import { Request } from 'express'
+import { NextApiRequest, NextPageContext } from 'next'
+import { parseCookies } from 'nookies'
+import { BASE_URL, TOKEN_COOKIES } from '../utils/constants'
 
-export const api = axios.create({
-  baseURL: BASE_URL,
-})
+export const createClientApi = (
+  ctx?:
+    | Pick<NextPageContext, 'req'>
+    | {
+        req: NextApiRequest
+      }
+    | {
+        req: Request
+      }
+    | null
+    | undefined,
+) => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+  })
+  const { [TOKEN_COOKIES]: token } = parseCookies(ctx)
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer: ${token}`
+  }
+
+  return api
+}
+
+export const api = createClientApi()
