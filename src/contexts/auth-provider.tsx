c9a0d737408c5c15f9f891132c9248ad9568a78f
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 00:14:13 -0300

    feat: add loading and error context

--- src/contexts/auth-provider.tsx
+++ src/contexts/auth-provider.tsx
@@ -1,12 +1,14 @@
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import Router from 'next/router'
 import { User } from '../interfaces/user'
-import { signIn, LoginData } from '../services/auth-service'
+import * as authSerivce from '../services/auth-service'
 
 interface Auth {
-  token?: string
   isAuthenticated: boolean
   user?: User
-  signIn: (data: LoginData) => void
+  signIn: (data: authSerivce.LoginData) => void
+  error?: string
+  loading?: boolean
 }
 
 type AuthProviderProps = {
@@ -15,8 +17,37 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as Auth)
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    authSerivce.recoveryUser().then((me) => setUser(me))
+  }, [])
+
+  async function signIn({ email, password }: authSerivce.LoginData) {
+    setError('')
+    setLoading(true)
+    const result = await authSerivce.signIn({ email, password })
+    if (result.success) {
+      setUser(result.data)
+      Router.push('/')
+    } else {
+      setError(result.error)
+    }
+    setLoading(true)
+  }
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated: false, signIn }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated: !!user,
+        user,
+        error,
+        signIn,
+        loading,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   )
