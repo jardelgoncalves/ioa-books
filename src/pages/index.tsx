58feaa0eac47dd4e5af4ba8ac294f33029658060
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 00:14:40 -0300

    feat: add redirect to login if token is not found

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
 import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
+import { TOKEN_COOKIES } from '../utils/constants'
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { [TOKEN_COOKIES]: token } = parseCookies(ctx)
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+      props: {},
+    }
+  }
+
   return {
     props: {
       title: 'Ioa Books',
