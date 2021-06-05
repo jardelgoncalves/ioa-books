1da925eab94dc517633ccc4c6b440e0f7facd9c5
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 13:13:57 -0300

    feat: add isFullPage layout

--- src/components/core/Layout/index.tsx
+++ src/components/core/Layout/index.tsx
@@ -3,10 +3,15 @@ import * as S from './styles'
 interface LayoutProps {
   children: React.ReactNode
   backgroundImage: string
+  isFullPage?: boolean
 }
 
-export const Layout = ({ children, backgroundImage }: LayoutProps) => (
-  <S.Layout>
+export const Layout = ({
+  children,
+  backgroundImage,
+  isFullPage,
+}: LayoutProps) => (
+  <S.Layout isFullPage={isFullPage}>
     <S.Content backgroundImage={backgroundImage}>{children}</S.Content>
   </S.Layout>
 )
