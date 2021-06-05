958093ec67ea7eabf24d2db6517e56812c28ff50
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 00:15:01 -0300

    feat: add redirect to home if token is found

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,19 @@
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
+import { TOKEN_COOKIES } from '../../utils/constants'
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { [TOKEN_COOKIES]: token } = parseCookies(ctx)
+  if (token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+      props: {},
+    }
+  }
+
   return {
     props: {
       title: 'Ioa Books | Login',
