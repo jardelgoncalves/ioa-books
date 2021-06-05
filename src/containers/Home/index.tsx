0c855c355917849382c2d65302034b4f72d027d5
jardel <jardelgoncalves1996@gmail.com>
2021-06-05 00:13:28 -0300

    feat: add user to header

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react'
 import { Header } from '../../components/core/Header'
 import { ListBooksInfinites } from '../../components/presentation/ListBooksInfinites'
+import { AuthContext } from '../../contexts/auth-provider'
 
 export default function Login() {
+  const { user } = useContext(AuthContext)
   return (
     <>
-      <Header user={{ name: 'John' }} />
+      <Header user={user} />
       <ListBooksInfinites />
     </>
   )
