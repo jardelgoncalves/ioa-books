5a5fb5f30d9342aefbea7a008b6bd1dad3a7d9cc
jardel <jardelgoncalves1996@gmail.com>
2021-06-06 15:49:43 -0300

    refactor: change style

--- src/components/core/Loading/styles.ts
+++ src/components/core/Loading/styles.ts
@@ -9,34 +9,22 @@ const ring = keyframes`
   }
 `
 
-export const Container = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  backdrop-filter: blur(4px);
-`
-
-export const Loading = styled.div`
+export const LoadingContainer = styled.div`
   display: inline-block;
   position: relative;
-  width: 80px;
-  height: 80px;
+  width: 1.375rem;
+  height: 1.375rem;
 
   div {
     box-sizing: border-box;
     display: block;
     position: absolute;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border: 8px solid ${({ theme }) => theme.colors.primary};
+    width: 1rem;
+    height: 1rem;
+    left: 0;
+    top: 0;
+    margin: 0.25rem;
+    border: 0.25rem solid ${({ theme }) => theme.colors.primary};
     border-radius: 50%;
     animation: ${ring} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
     border-color: ${({ theme }) => theme.colors.primary} transparent transparent
