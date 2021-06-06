587cefafc7a3426ff8ec4962e8264e378720a05e
jardel <jardelgoncalves1996@gmail.com>
2021-06-06 15:49:30 -0300

    refactor: remove one layer

--- src/components/core/Loading/index.tsx
+++ src/components/core/Loading/index.tsx
@@ -2,13 +2,11 @@ import * as S from './styles'
 
 export const Loading = () => {
   return (
-    <S.Container>
-      <S.Loading>
-        <div />
-        <div />
-        <div />
-        <div />
-      </S.Loading>
-    </S.Container>
+    <S.LoadingContainer>
+      <div />
+      <div />
+      <div />
+      <div />
+    </S.LoadingContainer>
   )
 }
