2e45d281cbc589e22c31e39e209be7cdccbd5a60
jardel <jardelgoncalves1996@gmail.com>
2021-06-07 23:44:50 -0300

    refactor: remove second type component

--- src/components/core/Quote/index.stories.tsx
+++ src/components/core/Quote/index.stories.tsx
@@ -17,9 +17,3 @@ export const Primary = Template.bind({})
 Primary.args = {
   text: 'The subject of “design thinking” is the rage at business schools, throughout corporations, and increasingly in the popular press—due in large part to the work of IDEO, a leading design firm, and its celebrated CEO, Tim Brown, who uses this book to show how the techniques and strategies of design belong at every level of business.',
 }
-
-export const Outline = Template.bind({})
-Outline.args = {
-  label: 'ok',
-  outline: true,
-}
