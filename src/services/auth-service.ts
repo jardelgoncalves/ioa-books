b27b228eb45ced8546a596c4a4d6aa4d68f73320
jardel <jardelgoncalves1996@gmail.com>
2021-06-08 00:36:36 -0300

    feat: add renew function and refresh token

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AxiosResponse } from 'axios'
 import { setCookie, parseCookies, destroyCookie } from 'nookies'
 import { User } from '../interfaces/user'
 import { api } from '../services/api'
@@ -5,6 +7,7 @@ import {
   TOKEN_COOKIES,
   USER_COOKIES,
   MAX_AGE_COOKIES,
+  REFRESH_TOKEN_COOKIES,
 } from '../utils/constants'
 
 export type LoginData = {
@@ -34,11 +37,20 @@ export const signIn = async ({
     setCookie(undefined, TOKEN_COOKIES, headers.authorization, {
       maxAge: MAX_AGE_COOKIES,
     })
+    setCookie(
+      undefined,
+      REFRESH_TOKEN_COOKIES,
+      JSON.stringify(headers['refresh-token']),
+      {
+        maxAge: MAX_AGE_COOKIES,
+      },
+    )
     setCookie(undefined, USER_COOKIES, JSON.stringify(data), {
       maxAge: MAX_AGE_COOKIES,
     })
 
     api.defaults.headers['Authorization'] = `Bearer ${headers.authorization}`
+    api.defaults.headers['refresh-token'] = `${headers['refresh-token']}`
   } catch (error) {
     result.success = false
     result.error = 'Ocorre um error inesperado'
@@ -57,4 +69,47 @@ export const recoveryUser = async (): Promise<User | null> => {
 export const logout = () => {
   destroyCookie(undefined, USER_COOKIES)
   destroyCookie(undefined, TOKEN_COOKIES)
+  destroyCookie(undefined, REFRESH_TOKEN_COOKIES)
+}
+
+const renewToken = async (ctx?: any) => {
+  const { [REFRESH_TOKEN_COOKIES]: refreshToken } = parseCookies(ctx)
+  const { headers } = await api.post('/auth/refresh-token', {
+    refreshToken,
+  })
+
+  api.defaults.headers['Authorization'] = `Bearer ${headers.authorization}`
+
+  setCookie(undefined, TOKEN_COOKIES, headers.authorization, {
+    maxAge: MAX_AGE_COOKIES,
+  })
+
+  setCookie(
+    undefined,
+    REFRESH_TOKEN_COOKIES,
+    JSON.stringify(headers['refresh-token	']),
+    {
+      maxAge: MAX_AGE_COOKIES,
+    },
+  )
+}
+
+export const refreshToken = async (
+  fn: () => Promise<AxiosResponse>,
+  ctx?: any,
+) => {
+  try {
+    return fn()
+  } catch (error) {
+    if (error.response) {
+      const { [REFRESH_TOKEN_COOKIES]: refreshToken } = parseCookies(ctx)
+
+      if (error.response.status === 401 && refreshToken) {
+        await renewToken()
+        return fn()
+      }
+    }
+
+    throw error
+  }
 }
