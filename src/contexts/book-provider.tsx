4297d3be3afaaec80e843d033886d25b2ca92524
jardel <jardelgoncalves1996@gmail.com>
2021-06-08 23:49:23 -0300

    feat: add book provider

---
+++ src/contexts/book-provider.tsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react'
+import { Book } from '../interfaces/book'
+
+interface BookProviderContext {
+  book: Book | null
+  onSelectBook?: (book: Book) => void
+}
+
+type BookProviderProps = {
+  children: React.ReactNode
+}
+
+export const BookContext = createContext({} as BookProviderContext)
+export const BookProvider = ({ children }: BookProviderProps) => {
+  const [book, setBook] = useState<Book | null>(null)
+
+  function onSelectBook(bookSelected: Book) {
+    setBook(bookSelected)
+  }
+
+  return (
+    <BookContext.Provider
+      value={{
+        book,
+        onSelectBook,
+      }}
+    >
+      {children}
+    </BookContext.Provider>
+  )
+}
