da852659ee8bd5e2549e4e7006cbf7295c4c4559
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-01 21:55:12 -0300

    refactor: adjust sizes

--- src/components/core/Quote/styles.ts
+++ src/components/core/Quote/styles.ts
@@ -2,19 +2,19 @@ import styled from 'styled-components'
 
 export const Paragraph = styled.p`
   width: 100%;
+  max-height: 6rem;
   position: relative;
   padding: 0.5rem 0;
   font-weight: ${({ theme }) => theme.typography.weight.regular};
   font-size: ${({ theme }) => theme.typography.sizes.xs};
   line-height: ${({ theme }) => theme.typography.lineHeight.base};
   color: ${({ theme }) => theme.colors.grey};
+  overflow-y: auto;
   text-indent: 1.5rem;
 
-  display: -webkit-box;
-  -webkit-line-clamp: 6;
-  -webkit-box-orient: vertical;
-  text-overflow: ellipsis;
-  overflow: hidden;
+  ${({ theme }) => theme.breakpoints.lessThan('medium')`
+    max-height: unset;
+  `}
 
   svg {
     content: '';
